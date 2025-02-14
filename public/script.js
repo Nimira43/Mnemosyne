a4a6b20f11c3e4034db35613b17cc41165921238
Nick Rabone <n.rabone72@aol.com>
2025-02-14 08:48:07 +0000

    Translate the trxt to speech

--- public/script.js
+++ public/script.js
@@ -2,3 +2,7 @@ const voiceSelect = document.getElementById('voiceSelect')
 const playButton = document.getElementById('playButton')
 const textInput = document.querySelector('textarea')
 
+playButton.addEventListener('click', () => {
+  const utterance = new SpeechSynthesisUtterance(textInput.value)
+  speechSynthesis.speak(utterance)
+})
