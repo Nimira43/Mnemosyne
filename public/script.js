4bc38ff36a857d89014a0bd725289c067daff992
Nick Rabone <n.rabone72@aol.com>
2025-02-14 09:02:28 +0000

    Function to load different voice types.

--- public/script.js
+++ public/script.js
@@ -2,6 +2,23 @@ const voiceSelect = document.getElementById('voiceSelect')
 const playButton = document.getElementById('playButton')
 const textInput = document.querySelector('textarea')
 
+let voices = []
+
+function loadVoices() {
+  voices = speechSynthesis.getVoices()
+  voiceSelect.innerHTML = voices
+  .map((voice, index) => 
+    `
+      <option value=${index}>
+        ${voice.name} ($voice.lang)
+      </option>
+    `
+  )
+  .join('')
+}
+
+
+
 playButton.addEventListener('click', () => {
   const utterance = new SpeechSynthesisUtterance(textInput.value)
   speechSynthesis.speak(utterance)
