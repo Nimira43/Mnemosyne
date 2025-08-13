60eb992ef85f6a041e1f3f060b8b7c705170492a
Nimira43 <n.rabone72@aol.com>
2025-08-13 19:14:55 +0100

    Taken out the body-form-item-wrapper from Body and placed into FormItem. Props setup.

--- src/components/FormItem.jsx
+++ src/components/FormItem.jsx
@@ -1,1 +1,17 @@
+export function FormItem({ label, name, checked, onChange }) {
+  return (
+    <div className='body-form-item-wrapper'>
+      <span className='body-form-checkbox-name'>{label}</span>
+      <label className='body-form-checkbox-container'>
+        <input
+          type='checkbox'
+          name={name}
+          checked={checked}
+          onChange={onChange}
+        />
+        <span className='body-form-checkmark'></span>
+      </label>
+    </div>
+  );
+}
 
