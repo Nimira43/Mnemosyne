d122d57c7b3f59b92bcfe950855fc0e3aaecddc9
Nimira43 <n.rabone72@aol.com>
2025-08-13 14:53:32 +0100

    Header styling

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,10 @@
+import { GiAbstract041 } from 'react-icons/gi'
+
 export function Header() {
   return (
-    <div>Header</div>
+    <div class='header-title'>
+      <GiAbstract041 className='logo-symbol' />
+      <h1 className='logo-text'>Mnemosyne</h1>
+    </div>
   )
 }
