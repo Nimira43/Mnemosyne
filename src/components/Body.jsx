9c32df7066b9150337f98ff20d73f23162f7e0f6
Nimira43 <n.rabone72@aol.com>
2025-08-13 15:41:07 +0100

    Form in Body - 1

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,7 +1,17 @@
 export function Body() {
   return (
     <form>
-      Form
+      <div>
+        <span>Size</span>
+        <select>
+        <option>8</option>
+        <option>10</option>
+        <option>15</option>
+        <option>20</option>
+      </select>
+      </div>
+      
+      <button>Invoke</button>
     </form>
   )
 }
