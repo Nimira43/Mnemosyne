df5729170f22220d885ad70d513affab2aa3803e
Nimira43 <n.rabone72@aol.com>
2025-08-13 16:20:48 +0100

    select styling.

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,17 +1,20 @@
 export function Body() {
   return (
-    <form>
-      <div>
+    <div className='body-container'>
+      <form >
+      <div  >
         <span>Size</span>
         <select>
         <option>8</option>
-        <option>10</option>
-        <option>15</option>
+        <option>12</option>
+        <option>16</option>
         <option>20</option>
       </select>
       </div>
       
       <button>Invoke</button>
     </form>
+    </div>
+    
   )
 }
