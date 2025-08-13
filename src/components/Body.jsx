0c575d57e727784e7463bf425dea710bf4937abf
Nimira43 <n.rabone72@aol.com>
2025-08-13 16:53:10 +0100

    More classNames applied to form elements. Checkbox markup prettyy much done.

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -4,8 +4,8 @@ export function Body() {
       <form className='body-form'>
         <div className='body-form-grid'>
           <div className='body-form-grid-item'>
-            <span>Size</span>
-            <select>
+            <span className='body-form-checkbox-name'>Size</span>
+            <select className='body-form-select'>
               <option>8</option>
               <option>12</option>
               <option>16</option>
@@ -13,16 +13,24 @@ export function Body() {
             </select>
           </div>
           <div className='body-form-grid-item'>
-            <span>Uppercase</span>
-            <input 
-              type="checkbox" 
-            />
+            <span className='body-form-checkbox-name'>Uppercase</span>
+            <label className="body-form-checkbox-container">
+              <input 
+                className="body-form-checkbox" 
+                type='checkbox'
+              />
+              <span class="body-form-checkmark"></span>
+            </label>
           </div>
           <div className='body-form-grid-item'>
-            <span>Numbers</span>
-            <input 
-              type="checkbox" 
-            />
+            <span className='body-form-checkbox-name'>Numbers</span>
+            <label className="body-form-checkbox-container">
+              <input 
+                className="body-form-checkbox" 
+                type='checkbox'
+              />
+              <span class="body-form-checkmark"></span>
+            </label>
           </div>
           <div className='body-form-grid-item'>
             <span className='body-form-checkbox-name'>Symbols</span>
