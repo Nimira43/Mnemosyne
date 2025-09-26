3a1501788f857e20c7fad1b97ee12a860715f19c
Nimira43 <n.rabone72@aol.com>
2025-09-26 20:18:01 +0100

    generatePassword - 1

--- src/lib/password.js
+++ src/lib/password.js
@@ -1,9 +1,21 @@
+function getRandonCharacter (text) {
+  Math.random() * text.length
+}
+
+
+
 export function generatePassword(config) {
   let result = ''
+  
   const lowercase = 'abcdefghijklmnopqrstuvwxyz'
   const uppercase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
   const numbers = '0123456789'
   const symbols = '!@#$%^&*()_-+=[]{}|;:\'",.<>?/\\'
 
+  let alphabet = lowercase
+
+  if (config.uppercase) {
+    alphabet += uppercase
+  }
   return result
 }
