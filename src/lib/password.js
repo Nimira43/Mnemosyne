b48fdb9473f114884f78671774efebdaa51822af
Nimira43 <n.rabone72@aol.com>
2025-09-26 20:37:28 +0100

    generatedPassword - 4

--- src/lib/password.js
+++ src/lib/password.js
@@ -4,6 +4,7 @@ function getRandonChar(text) {
 
 export function generatePassword(config) {
   let result = ''
+  let charToSkip = 1
   
   const LOWERCASE = 'abcdefghijklmnopqrstuvwxyz'
   const UPPERCASE = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
@@ -16,14 +17,23 @@ export function generatePassword(config) {
   if (config.uppercase) {
     alphabet += UPPERCASE
     result += getRandonChar(UPPERCASE)
+    charToSkip++
   }
+
   if (config.numbers) {
     alphabet += NUMBERS
     result += getRandonChar(NUMBERS)
+    charToSkip++
   }
+
   if (config.symbols) {
     alphabet += SYMBOLS
     result += getRandonChar(SYMBOLS)
+    charToSkip++
+  }
+
+  for (let i = charToSkip; i < Number(config.size); i++) {
+    result += getRandonChar(alphabet)
   }
   console.log(result)
   return result
