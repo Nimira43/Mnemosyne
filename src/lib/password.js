550708c6e82fbc48a29b4b57f0705b6c999f0151
Nimira43 <n.rabone72@aol.com>
2025-09-27 04:02:36 +0100

    Fisher Yates shuffle array - 5

--- src/lib/password.js
+++ src/lib/password.js
@@ -3,7 +3,11 @@ function shuffle(arr) {
 
   for (let i = array.length - 1; i > 0; i--) {
     let j = Math.floor(Math.random() * (i + 1))
+    let temp = array[i]
+    array[i] = array[j]
+    array[j] = temp
   }
+  return array
 }
 
 function getRandomChar(text) {
