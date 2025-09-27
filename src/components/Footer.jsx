55295cb162eada9ee803b8aa2f2e22aead8141e9
Nimira43 <n.rabone72@aol.com>
2025-09-27 04:17:08 +0100

    saveInClipboard functionality working. Project completed.

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,14 +1,15 @@
 import { MdContentPaste } from 'react-icons/md'
 
 export function Footer(props) {
-  function saveInClipboard() {}
+  function saveInClipboard() {
+    navigator.clipboard.writeText(props.password)
+  }
   
   return (
     <div 
       className='footer-container'
-      onClick={}
+      onClick={saveInClipboard}
     >
-      
       <h3>{props.password}</h3>
       <MdContentPaste />
     </div>
