5f143211d02dd2120c0a54a286e10999e8fca7f9
Nimira43 <n.rabone72@aol.com>
2025-09-27 04:11:37 +0100

    SaveInClipboard - 1

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,8 +1,14 @@
 import { MdContentPaste } from 'react-icons/md'
 
 export function Footer(props) {
+  function saveInClipboard() {}
+  
   return (
-    <div className='footer-container'>
+    <div 
+      className='footer-container'
+      onClick={}
+    >
+      
       <h3>{props.password}</h3>
       <MdContentPaste />
     </div>
